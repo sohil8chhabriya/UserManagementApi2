d68858e0c25e69a8b0cea88c740290a05852d65d
v-sohilc <v-sohilc@zillowgroup.com>
2018-09-12 11:46:40 +0530

    trying out AppUser Registration Implimentation

--- userapi/userDetails/views.py
+++ userapi/userDetails/views.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.http import JsonResponse
+from rest_framework.views import APIView
 from .models import AppUser
 
 from rest_framework import generics
@@ -7,13 +9,13 @@ from rest_framework import generics
 from . import models
 from . import serializers
 
-class CustomUserCreationForm(UserCreationForm):
+class AppUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         model = AppUser
         fields = ('first_name', 'last_name', 'email')
 
-class CustomUserChangeForm(UserChangeForm):
+class AppUserChangeForm(UserChangeForm):
 
     class Meta:
         model = AppUser
@@ -22,3 +24,22 @@ class CustomUserChangeForm(UserChangeForm):
 class UserListView(generics.ListCreateAPIView):
     queryset = models.AppUser.objects.all()
     serializer_class = serializers.UserSerializer
+
+class RegisterFormView(APIView):
+    def post(self, request, format=None):
+        user = models.AppUser.objects.all()
+        email = request.data.get("email", None)
+        first_name = request.data.get('first_name', None)
+        last_name = request.data.get('last_name', None)
+        password = request.data.get('password1', None)
+
+        user.first_name = first_name
+        user.last_name = last_name
+        user.email = email
+        user.password = password
+
+        try:
+            user.save()
+            return JsonResponse({'status': 1, 'message': 'User registered successfully!'})
+        except:
+            return JsonResponse({'status': 0, 'message': 'There was something wrong while registering the user.'})
